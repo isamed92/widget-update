04973d6db8c9c453c73b85c8e7374e9b6f39b4f4
Isaias Medina <isamed92@gmail.com>
2023-04-24 20:33:45 -0300

    name routes implemented

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/presentation/screens/screens.dart';
 
-final GoRouter AppRouter = GoRouter(
+final GoRouter appRouter = GoRouter(
   routes: <RouteBase>[
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (BuildContext context, GoRouterState state) {
         return const HomeScreen();
       },
       routes: <RouteBase>[
         GoRoute(
           path: 'buttons',
+          name: ButtonsScreen.name,
           builder: (BuildContext context, GoRouterState state) {
             return const ButtonsScreen();
           },
         ),
         GoRoute(
           path: 'cards',
+          name: CardsScreen.name,
           builder: (BuildContext context, GoRouterState state) {
             return const CardsScreen();
           },
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: AppRouter,
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme().getTheme(),
     );
